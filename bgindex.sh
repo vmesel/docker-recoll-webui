8a0e69520b86a73278d7f96a7a7e268db925f398
dsheyp <dsheyp@gmail.com>
2015-08-07 13:02:33 +0200

    Delete bgindex.sh

--- bgindex.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-while :
-do
-	/usr/bin/recollindex
-	startTime=$(date +%s)
-	endTime=$(date -d "tomorrow 0:00" +%s)
-	timeToWait=$(($endTime- $startTime))
-	/bin/sleep $timeToWait
-done
